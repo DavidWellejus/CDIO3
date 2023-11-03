bd9fa44dcdbdb12756c74640f2ef9bf06a2989d9
davidwellejus <davidwellejus@gmail.com>
2023-11-03 14:13:25 +0100

    Initial push

---
+++ cdio3/src/main/java/group2/cdio3/App.java
@@ -0,0 +1,13 @@
+package group2.cdio3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

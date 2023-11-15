d62eda425e0f7874f85d3dff7a1bad7917c177ed
davidwellejus <davidwellejus@gmail.com>
2023-11-15 14:39:50 +0100

    Changed the range to go from 1 to 6 in the rollDie function

--- cdio3/src/main/java/group2/cdio3/Die.java
+++ cdio3/src/main/java/group2/cdio3/Die.java
@@ -1,8 +1,9 @@
+package group2.cdio3;
 class Die {
     
     public static final int sides = 6;
     
     public static int rollDie(){
-        return (int (Math.random() * (sides - 1) + 1 ))
+        return (int) (Math.random() * (sides) +1);
     }
 }

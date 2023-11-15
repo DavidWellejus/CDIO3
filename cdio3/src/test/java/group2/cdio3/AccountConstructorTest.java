3b412495b6e97bd0bcf13dfb6754a0883dafbb72
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-11-15 14:41:14 +0100

    added account balance test template

---
+++ cdio3/src/test/java/group2/cdio3/AccountConstructorTest.java
@@ -0,0 +1,24 @@
+package group2.cdio3;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AccountConstructorTest {
+    
+
+        Account accountTest = new Account(1000);
+    
+    
+    @Test
+
+    public void AccountTest(){
+
+        assertEquals(1000, accountTest.*/* indsæt accountbalance her*/);
+        
+
+    }
+}

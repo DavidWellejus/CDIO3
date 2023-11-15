4870d8a7713a6008b9e4863c99a8f434a92a4374
davidwellejus <davidwellejus@gmail.com>
2023-11-15 21:30:05 +0100

    Created RollCupTest to check if the value of the die from rollCup is between 1 and 6

--- cdio3/src/test/java/group2/cdio3/RollCupTest.java
+++ cdio3/src/test/java/group2/cdio3/RollCupTest.java
@@ -1,5 +1,17 @@
 package group2.cdio3;
 
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertTrue;
 public class RollCupTest {
-    
+    @Test
+
+    public void diceSideTest(){
+        int min = 1;
+        int max = 6;
+        int side = RollCup.rollDie();
+        System.out.println(side);
+        assertTrue(side >= min && side <= max);
+    }
 }

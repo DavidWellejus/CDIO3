ec6907e9f7d06c71297003a5431727634665296e
Nikolaj <Nikolaj.Schaebel@gmail.com>
2023-11-15 14:46:19 +0100

    Added getAccountBalance, which returns the accountValue of the associated account

--- cdio3/src/main/java/group2/cdio3/Account.java
+++ cdio3/src/main/java/group2/cdio3/Account.java
@@ -1,7 +1,11 @@
-class Account{
+class Account {
    private int accountBalance;
 
-   public Account(int amount){
-    this.accountBalance = amount;
+   public Account(int amount) {
+      this.accountBalance = amount;
+   }
+
+   public int getAccountBalance() {
+      return this.accountBalance;
    }
 }

73801b64cbbf600ea59aaedc76efb6a1933d0f8f
davidwellejus <davidwellejus@gmail.com>
2023-11-17 12:39:51 +0100

    Created the method generateFieldArray which generates an array containing all the fields of the gameboard

--- cdio3/src/main/java/group2/cdio3/GameBoard.java
+++ cdio3/src/main/java/group2/cdio3/GameBoard.java
@@ -3,7 +3,7 @@ package group2.cdio3;
 class GameBoard {
 
     private Field[] fieldArray;
-    //private ChanceCard[] chanceCardArray;
+    private ChanceCard[] chanceCardArray;
     private Player[][] playerPosition;
     
     
@@ -32,5 +32,38 @@ class GameBoard {
         playerPosition[0][2] = player2;
         playerPosition[0][3] = player3;
         playerPosition[0][4] = player4;
+    }
+
+    private Field[] generateFieldArray(){
+
+        Field[] fieldArray = new Field[24];
+        
+        fieldArray[0] = new Start("Start", 0);
+        fieldArray[1] = new Property("Chum Bucket", 1);
+        fieldArray[2] = new Property("Krusty Krab", 2);
+        fieldArray[3] = new Chance("Chance", 3);
+        fieldArray[4] = new Property("Frozen YOYO", 4);
+        fieldArray[5] = new Property("Creapy Uncle's Crêpe", 5);
+        fieldArray[6] = new Jail("Jail", 6);
+        fieldArray[7] = new Property("Kinky Cinema",7);
+        fieldArray[8] = new Property("Colosseum", 8);
+        fieldArray[9] = new Chance("Chance", 9);
+        fieldArray[10] = new Property("Pole Dancing Studio", 10);
+        fieldArray[11] = new Property("Ole's Party Tub Club", 11);
+        fieldArray[12] = new Parking("Parking", 12);
+        fieldArray[13] = new Property("Dahlgaard's Tivoli", 13);
+        fieldArray[14] = new Property("My Little Pony Experience", 14);
+        fieldArray[15] = new Chance("Chance", 15);
+        fieldArray[16] = new Property("Asma's Corner Store", 16);
+        fieldArray[17] = new Property("Animal Farm", 17);
+        fieldArray[18] = new GoToJail("Go To Jail!", 18);
+        fieldArray[19] = new Property("Bowling and Carpets inc.", 19);
+        fieldArray[20] = new Property("Lobotomy Clinic", 20);
+        fieldArray[21] = new Chance("Chance", 21);
+        fieldArray[22] = new Property("Joe's Quick Suicide Clinic", 22);
+        fieldArray[23] = new Property("Restaurant Cold Feet", 23);
+        
+
+
     }
 }

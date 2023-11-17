964a2ba6c408fcd6586c7d352f6b57ab61ae20a7
davidwellejus <davidwellejus@gmail.com>
2023-11-17 11:50:41 +0100

    added empty constructors accepting between 2-4 players as arguement

--- cdio3/src/main/java/group2/cdio3/GameBoard.java
+++ cdio3/src/main/java/group2/cdio3/GameBoard.java
@@ -2,5 +2,22 @@ package group2.cdio3;
 
 class GameBoard {
 
+    private Field[] fieldArray;
+    //private ChanceCard[] chanceCardArray;
+    private int [][] playerPosition;
     
+    //Constructor for two players
+    public GameBoard(Player player1, Player player2){
+
+    }
+
+    //Constructor for three players
+    public GameBoard(Player player1, Player player2, Player player3){
+
+    }
+
+    //Constructor for four players
+    public GameBoard(Player player1, Player player2, Player player3, Player player4){
+
+    }
 }

ac127762af5766ea897eb77eb82ad0b303383233
Klavs <s213383@dtu.dk>
2023-11-17 13:25:43 +0100

    samme som før

--- cdio3/src/test/java/group2/cdio3/PropertyTest.java
+++ cdio3/src/test/java/group2/cdio3/PropertyTest.java
@@ -1,1 +1,14 @@
 package group2.cdio3;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+class PropertyTest{
+    Player player1 = new Player("cat", 20, 'C');
+    Property test = new Property(2, false, "blue", player1, 2, "Pizza");
+
+@Test
+public void getDescriptionTest(){
+    assertEquals("blue Pizza 2",test.getDescription());
+}
+}

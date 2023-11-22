a8bbc9bcd0876ce873104453f7de1aeb930d0738
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-11-22 16:05:45 +0100

    Delete cdio3/src/main/java/group2/cdio3 directory

--- cdio3/src/main/java/group2/cdio3/Die.java
+++
@@ -1,9 +0,0 @@
-package group2.cdio3;
-class Die {
-    
-    public static final int sides = 6;
-    
-    public static int rollDie(){
-        return (int) (Math.random() * (sides) +1);
-    }
-}

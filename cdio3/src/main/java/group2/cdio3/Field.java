149b4224dd17726b7dd59a0e64268c0dd1591fdc
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-11-22 12:15:06 +0100

    added price as a field in the Field class

--- cdio3/src/main/java/group2/cdio3/Field.java
+++ cdio3/src/main/java/group2/cdio3/Field.java
@@ -4,10 +4,12 @@ abstract class Field {
 
     public final String name;
     public final int fieldNumber;
+    public int price;
 
     public Field(String name, int fieldNumber) {
         this.name = name;
         this.fieldNumber = fieldNumber;
+        this.price = 0;
     }
 
     public String getFieldColour() {
